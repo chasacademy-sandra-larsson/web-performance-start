20767097ee35b578d405764a46b892d76bf2ce16
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2024-11-20 09:33:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Large JavaScript File to Simulate Render Blocking
+console.log('Render-blocking script running...');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
+});
